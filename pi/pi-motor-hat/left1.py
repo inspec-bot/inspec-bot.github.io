05d4f02209045bdda54d8b220893cd757d8e7806
inspecbot <67633423+inspecbot@users.noreply.github.com>
2020-08-24 10:15:04 -0700

    Update left1.py

--- pi/pi-motor-hat/left1.py
+++ pi/pi-motor-hat/left1.py
@@ -15,25 +15,17 @@ mh = Raspi_MotorHAT(addr=0x6f)
 #atexit.register(turnOffMotors)
 
 # motor-config
-myMotor1 = mh.getMotor(2)
+myMotor1 = mh.getMotor(3)
 myMotor2 = mh.getMotor(4)
 
-# set speed 0 (off) to 255 (max speed)
+# set speed 0 (off) to 255 (max speed) 256/x-1
 # 40/80 min (unbalance motor)
-myMotor1.setSpeed(150)
-myMotor2.setSpeed(150)
+myMotor1.setSpeed(127)
+myMotor2.setSpeed(127)
 # turn on motor
 myMotor1.run(Raspi_MotorHAT.RELEASE);
 myMotor2.run(Raspi_MotorHAT.RELEASE);
 
-while (True):
-  print ("Forward! ")
-  myMotor1.run(Raspi_MotorHAT.FORWARD)
-  myMotor2.run(Raspi_MotorHAT.BACKWARD)
-  time.sleep(0.5)
-
-  print ("Release")
-  myMotor1.run(Raspi_MotorHAT.RELEASE)
-  myMotor2.run(Raspi_MotorHAT.RELEASE)
-  time.sleep(0.5)
+myMotor1.run(Raspi_MotorHAT.FORWARD)
+myMotor2.run(Raspi_MotorHAT.BACKWARD)
 
